6f3826c9dcf595d847485bd207df2612a52c8e29
gatsby6060 <uniline123@naver.com>
2025-09-30 01:41:11 +0900

    엔터하나추가

--- test1/src/main/java/com/example/test1/model/Member.java
+++ test1/src/main/java/com/example/test1/model/Member.java
@@ -4,11 +4,13 @@ import lombok.Data;
 
 @Data
 public class Member {
+	
 	private String userId;
 	private String password;
 	private String name;
 	private String birth;
 	private String nickName;
 	private String status;
+	
 	
 }
